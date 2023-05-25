8c0ff4aee818c19867dc9693e3bab66c8a1862f3
Filipe Pina <fopina@gmail.com>
2023-05-26 00:19:17 +0100

    flag to print out request and response when upstream returns 4xx/5xx

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var userAgent string
 var ja3 string
 var listenAddress string
 var timeout int
+var printErrors bool
 
 func writeError(w http.ResponseWriter, err error) {
 	w.WriteHeader(500)
@@ -61,6 +62,14 @@ func hello(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	if printErrors && (response.Status >= 400) {
+		log.Printf("Response status %d", response.Status)
+		log.Printf("== request ==")
+		log.Printf("%v", req)
+		log.Printf("== response ==")
+		log.Printf("%v", response)
+	}
+
 	w.WriteHeader(response.Status)
 	for name, h := range response.Headers {
 		w.Header().Add(name, h)
@@ -77,6 +86,7 @@ func main() {
 	flag.StringVar(&ja3, "ja3", "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0", "JA3 token to spoof, should align with user-agent")
 	flag.StringVar(&listenAddress, "bind", "127.0.0.1:8888", "Listening address to bind to")
 	flag.IntVar(&timeout, "timeout", 60, "Request timeout")
+	flag.BoolVar(&printErrors, "print-errors", false, "Print request and response when an error (4xx and 5xx) is returned from upstream server")
 	versionPtr := flag.Bool("version", false, "display version")
 
 	flag.Usage = func() {
