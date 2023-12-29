f7b14951df63aae6d1f99ae45e251a63f30d614d
Filipe Pina <hzlu1ot0@duck.com>
2023-12-29 02:10:33 +0000

    add `-upstream-proxy` flag

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ var ja3 string
 var listenAddress string
 var timeout int
 var printErrors bool
+var upstreamProxy string
 
 func writeError(w http.ResponseWriter, err error) {
 	w.WriteHeader(500)
@@ -56,6 +57,7 @@ func hello(w http.ResponseWriter, req *http.Request) {
 		UserAgent: userAgent,
 		Headers:   forwardedHeaders,
 		Timeout:   timeout,
+		Proxy:     upstreamProxy,
 	}, req.Method)
 	if err != nil {
 		writeError(w, err)
@@ -85,6 +87,7 @@ func main() {
 	flag.StringVar(&userAgent, "ua", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0", "User-Agent to spoof, should align with JA3 token")
 	flag.StringVar(&ja3, "ja3", "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0", "JA3 token to spoof, should align with user-agent")
 	flag.StringVar(&listenAddress, "bind", "127.0.0.1:8888", "Listening address to bind to")
+	flag.StringVar(&upstreamProxy, "upstream-proxy", "", "Upstream proxy (if any required)")
 	flag.IntVar(&timeout, "timeout", 60, "Request timeout")
 	flag.BoolVar(&printErrors, "print-errors", false, "Print request and response when an error (4xx and 5xx) is returned from upstream server")
 	versionPtr := flag.Bool("version", false, "display version")
